15a36c44d5281c9dfdff0441ce4986609a2b6ebb
Owen Lewis <owenalewis@gmail.com>
2023-09-19 15:45:26 +0100

    Writes code for analyzeArray function

--- analyzeArray/analyzeArray.js
+++ analyzeArray/analyzeArray.js
@@ -1,3 +1,16 @@
-const analyzeArray = (arr) => {};
+const analyzeArray = (arr) => {
+  const sum = arr.reduce((acc, val) => acc + val, 0);
+  const average = sum / arr.length;
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const length = arr.length;
+
+  return {
+    average,
+    min,
+    max,
+    length,
+  };
+};
 
 export default analyzeArray;
