f1de31e69500bbaf902949aaf66a2663315fa687
Owen Lewis <owenalewis@gmail.com>
2023-09-19 15:37:05 +0100

    Creates tests

---
+++ caeserCipher/caeserCipher.test.js
@@ -0,0 +1,22 @@
+import caesarCipher from "./caesarCipher";
+
+test("shifts characters by a given factor", () => {
+  expect(caesarCipher("abc", 1)).toBe("bcd");
+  expect(caesarCipher("xyz", 1)).toBe("yza");
+});
+
+test("wraps from z to a", () => {
+  expect(caesarCipher("z", 1)).toBe("a");
+});
+
+test("keeps the same case", () => {
+  expect(caesarCipher("AbC", 1)).toBe("BcD");
+});
+
+test("handles punctuation and spaces", () => {
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
+
+test("handles negative shifts", () => {
+  expect(caesarCipher("bcd", -1)).toBe("abc");
+});
